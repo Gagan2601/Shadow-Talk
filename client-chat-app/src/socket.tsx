536074e10cf92c9b2c31171d44b230136b2822d2
Gagan2601 <deepgagan9956@gmail.com>
2024-10-21 16:12:28 +0530

    Added Rooms Page

--- client-chat-app/src/socket.tsx
+++ client-chat-app/src/socket.tsx
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000"); //https://shadow-talk.onrender.com
+export const socket = io("https://shadow-talk.onrender.com"); //
